4e4f20c2b24054779e464f8a618a481584d102fb
Aleksey-Kolyasa <aleksey.kolyasa@cloudletlabs.com>
2017-04-01 06:09:50 +0300

    - some refortating

--- app/js/app/guntt/controllers/gunttUserAndResourceController.js
+++ app/js/app/guntt/controllers/gunttUserAndResourceController.js
@@ -2,17 +2,15 @@ define(['angular'], function (angular) {
     'use strict';
 
     /* DEFINE USER AND RESOURCES CONTROLLER MODULE */
-   var gunttUserAndResourcesCtrlModule = angular.module('gunttUserAndResourcesCtrlModule',[]);
+    var gunttUserAndResourcesCtrlModule = angular.module('gunttUserAndResourcesCtrlModule', []);
 
-   /*USER AND RESOURCES APP CONTROLLER **START** */
-   gunttUserAndResourcesCtrlModule.controller('gunttUserAndResourcesCtrl', function ($scope, $window, $document, $log, toastr) {
+    /*USER AND RESOURCES APP CONTROLLER **START** */
+    gunttUserAndResourcesCtrlModule.controller('gunttUserAndResourcesCtrl', function ($scope, $window, $document, $log, toastr) {
 
         toastr.success('TEST: Wellcome to Guntt!');
 
+        /*USER AND RESOURCES APP CONTROLLER **END** */
 
-    /*USER AND RESOURCES APP CONTROLLER **END** */
-
-    return gunttUserAndResourcesCtrlModule;
-    /*END*/
+        return gunttUserAndResourcesCtrlModule;
     });
 });
